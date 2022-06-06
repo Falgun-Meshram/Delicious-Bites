f13017528186ffc1f1b969e3766041e47f8cdb63
Falgun Meshram <fdmeshra@uwaterloo.ca>
2022-06-06 11:47:43 -0400

    Added load balancing

--- start.js
+++ start.js
@@ -19,7 +19,14 @@ require('./models/Review');
 
 // Start our app!
 const app = require('./app');
-app.set('port', process.env.PORT || 7777);
+const port = process.argv ? parseInt(process.argv[2]) : null;
+app.set('port', port || 7779);
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Express running → PORT ${server.address().port}`)
 });
+
+// const app2 = require('./app');
+// app2.set('port', 7777);
+// const server2 = app2.listen(app2.get('port'), () => {
+//   console.log(`Express running → PORT ${server2.address().port}`);
+// }).catch(console.log('aglfhaldfjhadslhydalfjdhflahjsdfljdfahd'))
